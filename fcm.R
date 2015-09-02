ba176961d30d4db3d830b2ca7d4a063470bac24b
Daria Kopaliani <daria.kopaliani@gmail.com>
2015-09-02 18:59:04 +0300

    minor refactoring

--- fcm.R
+++ fcm.R
@@ -6,26 +6,26 @@ vector.norm <- function(x) {
 fcm.PC <- function(fcm) {
   
   PC <- 0
-  for (i in 1 : nrow(fcm$U)) {
-    for (j in 1 : ncol(fcm$U)) {
-      PC <- PC + fcm$U[i, j]^2
+  for (i in 1 : nrow(fcm$membership.values)) {
+    for (j in 1 : ncol(fcm$membership.values)) {
+      PC <- PC + fcm$membership.values[i, j]^2
     }
   }
   
-  PC / nrow(fcm$U)
+  PC / nrow(fcm$membership.values)
 }
 
 fcm.XB <- function(fcm, data) {
   
   NXB <- 0 
   for (i in 1 : nrow(data)) {
-    for (j in 1 : ncol(fcm$U)) {
+    for (j in 1 : ncol(fcm$membership.values)) {
       NXB <- fcm$U[i, j]^(fcm$fuzzifier) * vector.norm(data[i,] - fcm$centers[j,])
     }
   }
   NXB <- NXB / nrow(data)
   
-  DXB <- 1000
+  DXB <- .Machine$integer.max
   for (i in 1 : nrow(fcm$centers)) {
     for (i in 1 : nrow(fcm$centers)) {
       if (i != j) {
@@ -43,56 +43,53 @@ fcm.XB <- function(fcm, data) {
 fcm.membership.values <- function(data, centers, fuzzifier) {
   
   sample <- as.matrix(data)
-  u <- matrix(0, nrow = nrow(sample), ncol = nrow(centers))
+  membership.values <- matrix(0, nrow = nrow(sample), ncol = nrow(centers))
   for (i in 1 : nrow(sample)) {
     for (j in 1 : nrow(centers)) {
-      u[i, j] <- vector.norm(sample[i,] - centers[j,])^(2 / (1 - fuzzifier))
+      membership.values[i, j] <- vector.norm(sample[i,] - centers[j,])^(2 / (1 - fuzzifier))
       z <- 0
       for (k in 1 : nrow(centers)) {
         z <- z + vector.norm(sample[i,] - centers[k,])^(2 / (1 - fuzzifier))
       }
-      u[i, j] <- u[i, j] / z
+      membership.values[i, j] <- membership.values[i, j] / z
     }
   }
-  u
+  membership.values
 }
 
-fcm.online.run <- function(data, nclusters, m = 2, ee = 0.01, centers = NULL) {
+fcm.online.run <- function(data, nclusters, fuzzifier = 2, centers = NULL) {
   
   if (is.null(centers)) {
     centers <- cbind(runif(nclusters, 0, 1), runif(nclusters, 0, 1))  
   } 
-  fcm <- list(centers = centers, fuzzifier = m, U = matrix(0, nrow = nrow(data), ncol = nclusters))  
+  fcm <- list(centers = centers, fuzzifier = fuzzifier, membership.values = NULL)  
   for (i in 1 : nrow(data)) {
     sample <- as.matrix(data[i,])
     ss <- 0.6 * exp(-(i/nrow(data)))
-    u <- fcm.membership.values(sample, fcm$centers, m)
+    u <- fcm.membership.values(sample, fcm$centers, fuzzifier)
     for (j in 1 : nclusters)  {
-      fcm$centers[j,] <- fcm$centers[j,] + ss * (u[1, j] ^ m) * (sample - fcm$centers[j,])
+      fcm$centers[j,] <- fcm$centers[j,] + ss * (u[1, j] ^ fuzzifier) * (sample - fcm$centers[j,])
     }
   }
-  fcm$U <- fcm.membership.values(data, fcm$centers, m)  
+  fcm$membership.values <- fcm.membership.values(data, fcm$centers, fuzzifier)  
 
   fcm
 }
 
-fcm.batch.run <- function(data, nclusters, fuzzifier = 2, centers = NULL, ee = 0.01) {
+fcm.batch.run <- function(data, nclusters, fuzzifier = 2, e = 0.01, max.epoch = 100) {
   
-  if (is.null(centers)) {
-    centers <- cbind(runif(nclusters, 0, 1), runif(nclusters, 0, 1))  
-  }
-  U <- matrix(0, nrow = nrow(data), ncol = nclusters)
-  
-  for (k in 1 : 100) {
-    prevU <- U
-    U <- fcm.membership.values(data, centers, fuzzifier)    
+  centers <- cbind(runif(nclusters, 0, 1), runif(nclusters, 0, 1))  
+  membership.values <- matrix(0, nrow = nrow(data), ncol = nclusters)
+  for (k in 1 : max.epoch) {
+    prev.membership.values <- membership.values
+    membership.values <- fcm.membership.values(data, centers, fuzzifier)    
     for (j in 1 : nclusters) {
-      centers[j,] <- apply((U[,j] ^ fuzzifier) * data, 2, sum) / sum(U[,j] ^ fuzzifier)
+      centers[j,] <- apply((membership.values[,j] ^ fuzzifier) * data, 2, sum) / sum(membership.values[,j] ^ fuzzifier)
     }
-    if (max(abs(prevU - U)) < ee)  {
+    if (max(abs(prev.membership.values - membership.values)) < e)  {
       break
     }
   }
   
-  list(centers = centers, fuzzifier = m, U = U)
+  list(centers = centers, fuzzifier = fuzzifier, membership.values = membership.values)
 }
