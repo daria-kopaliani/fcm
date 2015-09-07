35914673b2dc16bc3dadf31daf9a256a36504934
Daria Kopaliani <daria.kopaliani@gmail.com>
2015-09-07 21:26:32 +0300

    playground_wine updated

--- playground_wine.R
+++ playground_wine.R
@@ -1,6 +1,7 @@
 library("HDclassif")
 source("fcm.R")
 
+data(wine)
 benchmark <- wine
 data <- apply(benchmark[, -1], 2, function(x){(x-min(x))/(max(x)-min(x))})
 order <- sample(nrow(data))
@@ -12,12 +13,7 @@ classes[wine[,1] == 3,] <- "class3"
 benchmark1 <- cbind(classes, wine)
 
 n_clusters <- 7
-fuzzifier <- 2
-centers <- fcm.batch.run(data[1:20,], n_clusters, fuzzifier)$centers
-fcm <- fcm.online.run(data, n_clusters, fuzzifier = fuzzifier, centers = centers)
+fuzzifier <- 3
+fcm <- fcm.init(n_clusters, fuzzifier, ncol(data), data[1:20,])
+fcm <- fcm.online.run(fcm, data)
 clustering.accuracy(fcm.cluster(fcm, data), benchmark1[order, 1])
-
-clusters <- fcm.cluster(fcm, data)
-labels <- benchmark[order, 1]
-
-tt <- cbind(clusters, as.matrix(labels))
